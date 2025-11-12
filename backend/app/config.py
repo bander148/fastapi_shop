82f0fddace34bf563d359dfa988279aa714e83a5
ban.der4 <ctepanskinder@yandex.ru>
2025-11-12 19:22:20 +0300

    init

---
+++ backend/app/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # название приложения
+    app_name: str = "FastAPI Shop"
+    # позволяет видеть ошибки
+    debug: bool = False
+    database_uri: str = "sqlite:///./shop.db"
+    # пути от которых принимаются бэкенд запросы
+    cors_origins: list = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000"
+    ]
+    static_dir: str = "static"
+    images_dir: str = "static/images"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ backend/app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from .config import settings
+
+engine = create_engine(
+    # получаем database url который указан в настройках
+    settings.database_url,
+    connect_args={"check_same_thread": False}
+)
+# создает сессии на соединение базы данных
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+     # вызывает новую сессию когда нужна база данных
+    db = SessionLocal()
+    try:
+        # пытается передать базу данных
+        yield db
+    finally:
+        #в конце закрывает сессию
+        db.close()
+
+def init_db():
+    #иницилизирует базу данных с помощью engine
+    Base.metadata.create_all(bind=engine)
