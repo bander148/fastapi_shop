795e2e11c0c7ff14543d18bbf1be514ad45f756e
ban.der4 <ctepanskinder@yandex.ru>
2025-11-15 12:46:11 +0300

    add schemas/repositories/services

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,22 +6,24 @@ from .config import settings
 engine = create_engine(
     # получаем database url который указан в настройках
     settings.database_url,
+    # нельзя делать несколько запросов ондовременно
     connect_args={"check_same_thread": False}
 )
 # создает сессии на соединение базы данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# описание таблиц / бд
 Base = declarative_base()
 
 def get_db():
      # вызывает новую сессию когда нужна база данных
     db = SessionLocal()
     try:
-        # пытается передать базу данных
+        # ищет нужную информацию
         yield db
     finally:
         #в конце закрывает сессию
         db.close()
 
 def init_db():
-    #иницилизирует базу данных с помощью engine
+    # иницилизирует базу данных с помощью engine
     Base.metadata.create_all(bind=engine)
